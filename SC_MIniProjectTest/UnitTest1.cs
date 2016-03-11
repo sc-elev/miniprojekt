cc0d629bae8ff64e121ba4650cf623b1545bd2ad
John Doe <john.doe@nowhere.net>
2016-03-11 07:58:27 +0100

    Scoreboard backend in place.

--- SC_MIniProjectTest/UnitTest1.cs
+++ SC_MIniProjectTest/UnitTest1.cs
@@ -20,6 +20,7 @@ namespace SC_MIniProjectTest
             string[] result = ConfigFile.GetFruits();
         }
 
+
         [TestMethod]
         public void SentenceCtrlReturnsModel()
         {
@@ -29,5 +30,43 @@ namespace SC_MIniProjectTest
             Assert.AreEqual("En konstig mening, eller hur?", model.original);
             Assert.AreEqual("En konstig mening* eller hur*", model.visible);
         }
+
+
+        [TestMethod]
+        public void ScoreboardSortsAndLimitValues()
+        {
+            var db = new TestScoreDB();
+            var scoreboard = new Scoreboard(db);
+            scoreboard.AddScore("foo", 10);
+            scoreboard.AddScore("bar", 20);
+            scoreboard.AddScore("baz", 6);
+            scoreboard.AddScore("baz1", 6);
+            scoreboard.AddScore("baz2", 6);
+            scoreboard.AddScore("baz3", 6);
+            scoreboard.AddScore("baz4", 6);
+            scoreboard.AddScore("baz5", 6);
+            scoreboard.AddScore("baz6", 6);
+            scoreboard.AddScore("baz7", 6);
+            scoreboard.AddScore("baz8", 6);
+            scoreboard.AddScore("baz9", 6);
+
+            var holders = scoreboard.GetScores();
+            Assert.AreEqual("bar",holders[0].Nickname );
+            Assert.AreEqual(10, holders.Count);
+        }
+
+        [TestMethod]
+        public void ScoreBoardRemovesDups()
+        {
+            var db = new TestScoreDB();
+            var scoreboard = new Scoreboard(db);
+            scoreboard.AddScore("foo", 6);
+            scoreboard.AddScore("foo", 10);
+            Assert.AreEqual(1, scoreboard.GetScores().Count);
+            Assert.AreEqual(10, scoreboard.GetScores()[0].Score);
+            Assert.AreEqual("foo", scoreboard.GetScores()[0].Nickname);
+        }
     }
+
+
 }
---
+++ SC_MiniProject/DAL/Scoreboard.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SC_MiniProject.DAL
+{
+    public  class ScoreHolder
+    {
+        public int Score { set; get;  }
+        public string Nickname { set; get;  }
+
+        public ScoreHolder() { }
+
+        public ScoreHolder(string nick, int score)
+        {
+            Nickname = nick;
+            Score = score;
+        }
+    }
+
+
+    public interface ScoreDB
+    {
+        IList<ScoreHolder> Get();
+        void Set(IList<ScoreHolder> holders);
+    }
+
+
+    public class SessionScoreDB: ScoreDB
+    {
+        public IList<ScoreHolder> Get()
+        {
+            if (HttpContext.Current.Session == null ||
+                HttpContext.Current.Session["scores"] == null)
+            {
+                Set(new List<ScoreHolder>());
+            }
+            return (IList<ScoreHolder>)HttpContext.Current.Session["scores"];
+        }
+
+
+        public void Set(IList<ScoreHolder> holders)
+        {
+            HttpContext.Current.Session["scores"] = holders;
+        }
+    }
+
+
+    public class TestScoreDB: ScoreDB
+    {
+        IList<ScoreHolder> holders = new List<ScoreHolder>();
+
+        public IList<ScoreHolder> Get()
+        {
+            return holders;
+        }
+
+        public void Set(IList<ScoreHolder> holders)
+        {
+            this.holders = holders;
+        }
+    }
+
+
+    public class Scoreboard
+    {
+        protected static int SIZE = 10;
+
+        protected ScoreDB db;
+
+
+        public IList<ScoreHolder> GetScores()
+        {
+            return db.Get();
+        }
+
+
+        public void SetScores(IList<ScoreHolder> holders)
+        {
+            db.Set(holders);
+        }
+
+
+        public void AddScore(string nick, int score)
+        {
+            var holders = db.Get();
+            var dup = holders.Where(h => h.Nickname == nick).FirstOrDefault();
+            if (dup != null)
+            {
+                if (dup.Score > score)
+                    return;
+                holders.Remove(dup);
+            }
+            holders.Add(new ScoreHolder(nick, score));
+            holders = holders.OrderByDescending(h => h.Score).ToList();
+            if (holders.Count > SIZE)
+                holders.RemoveAt(holders.Count - 1);
+            db.Set(holders);
+        }
+
+
+        public Scoreboard()
+        {
+            db = new SessionScoreDB();
+        }
+
+
+        public Scoreboard(ScoreDB scoreDB)
+        {
+            db = scoreDB;
+        }
+    }
+}
